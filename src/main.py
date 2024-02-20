53aa313cef188bae3ef442be7c626cf57ecd84b4
ANIL SINGLA <anilsingla111@gmail.com>
2024-02-20 13:03:25 +0530

    Update main.py

--- src/main.py
+++ src/main.py
@@ -1,1 +1,19 @@
+import system
+import argparse
+import loguru
 
+from pentestMain import pentestInit
+
+def main():
+  """ Initialize the pentest process """
+  # Initialize Argument Parser to parse the command line arguments
+  argumentParser = argparse.ArgumentParser(description="Argument parser to parse arguments.")
+
+  argumentParser.add_argument(
+    "--log-dir",
+    type=str,
+    default="logs",
+    help="Path to the directory to store the session logs",
+  )
+
+  
